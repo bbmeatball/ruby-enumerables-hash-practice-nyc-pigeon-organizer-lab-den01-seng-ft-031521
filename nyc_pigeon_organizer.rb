82f414bfdbc631e14800594cf2c22cab80879e54
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-02-20 21:51:56 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   data.each_with_object({})do|(key,value), final_array|
-value.each.do |inner_key,names|
+value.each.do |inner_key, names|
   names.each.do |name|
+    if !final_array[name]
+      finally[name]={}
 end
 end
  final_array 
