ec47b00ecb20db0aaf9628a3a5f05856233450d8
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-02-20 22:05:50 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,9 +2,9 @@
 
 def nyc_pigeon_organizer(data)
 new_hash={}
-data.each.do |key,value|
-  value.each.do |new_value, names|
-    names.each.do |name|
+data.each do |key, value|
+  value.each do |new_value, names|
+    names.each do |name|
       if !new_hash[name]
         new_hash[name]={}
       end
