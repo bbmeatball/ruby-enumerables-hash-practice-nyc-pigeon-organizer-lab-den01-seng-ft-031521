a7b638c38dc4cfc32ce0d5ac36c57434b565ada6
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-02-20 22:04:08 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,18 +1,19 @@
 
 
 def nyc_pigeon_organizer(data)
-  
-  final_results=data.each_with_object({})do |(key,value), final_array|
-value.each.do |inner_key, names|
-  names.each.do |name|
-    if !final_array[name]
-      finally[name]={}
+new_hash={}
+data.each.do |key,value|
+  value.each.do |new_value, names|
+    names.each.do |name|
+      if !new_hash[name]
+        new_hash[name]={}
+      end
+      if !new_hash[name][key]
+        new_hash[name][key]=[]
+      end
+      new_hash[name][key]<< new_value.to_s
+    end
+  end
 end
-if !final_array[name][key]
-  !final_array[name][key]=[]
-end
-final_array[name][key].push(inner_key)
-end
-end
-end
+new_hash
 end
